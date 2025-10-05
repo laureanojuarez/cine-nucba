3c9035017fe8472bc511025098abaa5a463a8031
Laureano <laureanojuarez69@gmail.com>
2025-10-05 17:27:58 -0300

    refactor: replace axios instance with direct axios calls in films API and update Home component for film display

--- cine-frontend/src/api/films.ts
+++ cine-frontend/src/api/films.ts
@@ -1,25 +1,22 @@
-import api from "./axios.js";
+import axios from "axios";
+
+const API_KEY = "95145abf695701fea57e4211f53dc98e";
 
 export const fetchFilms = async () => {
   try {
-    const response = await api.get("/api/films");
-    return response.data;
+    const {data} = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie`,
+      {
+        params: {
+          api_key: API_KEY,
+        },
+      }
+    );
+    console.log("fetchFilms data:", data);
+    console.log("fetchFilms results:", data.results);
+    return data.results;
   } catch (error) {
     console.error("Error fetching films:", error);
-    throw new Error("Error al cargar películas");
-  }
-};
-
-export const fetchFilmById = async (id) => {
-  try {
-    const response = await api.get(`/api/films/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching film by id:", error);
-
-    if (error.response?.status === 404) {
-      throw new Error("Película no encontrada");
-    }
-    throw new Error("Error al cargar película");
+    return [];
   }
 };
--- cine-frontend/src/pages/Home/Home.tsx
+++ cine-frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+import {useEffect, useState} from "react";
+import {fetchFilms} from "../../api/films";
+
+export default function App() {
+  const [films, setFilms] = useState<any[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const results = await fetchFilms();
+      console.log("fetchFilms (desde App):", results);
+      setFilms(results || []);
+    };
+    load();
+  }, []);
+
   return (
-    <main>
-      <h1>HOLA</h1>
+    <main className="">
+      <section className="flex flex-col gap-2">
+        <h1 className="font-bold text-2xl">Peliculas en cartelera</h1>
+        <ul className="flex flex-wrap gap-4 list-none p-0">
+          {films.map((f) => (
+            <li key={f.id} className="w-61">
+              <figure className="bg-gray-800 rounded overflow-hidden">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${f.poster_path}`}
+                  alt={f.title}
+                  className="w-full h-auto"
+                  loading="lazy"
+                />
+                <figcaption className="text-center p-2">
+                  <div className="font-medium">{f.title}</div>
+                  <div className="text-sm text-gray-300">
+                    {f.release_date ?? "—"} · {f.vote_average ?? "—"}
+                  </div>
+                </figcaption>
+              </figure>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
