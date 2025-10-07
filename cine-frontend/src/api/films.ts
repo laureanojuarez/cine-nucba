a3b0fff196e16b240a91c2e19eee8702215fdb86
Laureano <laureanojuarez69@gmail.com>
2025-10-07 10:09:30 -0300

    feat: Update API key handling and add .env configuration for secure storage

--- cine-frontend/src/api/films.ts
+++ cine-frontend/src/api/films.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_KEY = "95145abf695701fea57e4211f53dc98e";
+const API_KEY = import.meta.env.TMDB_API_KEY;
 const LANGUAGE = "es-ES";
 
 export const fetchFilms = async () => {
   try {
-    const { data } = await axios.get(
+    const {data} = await axios.get(
       `https://api.themoviedb.org/3/discover/movie`,
       {
         params: {
@@ -23,15 +23,12 @@ export const fetchFilms = async () => {
 
 export const fetchFilmById = async (id: string) => {
   try {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/movie/${id}`,
-      {
-        params: {
-          api_key: API_KEY,
-          language: LANGUAGE,
-        },
-      }
-    );
+    const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        language: LANGUAGE,
+      },
+    });
     return data;
   } catch (error) {
     console.error("Error fetching film by id:", error);
