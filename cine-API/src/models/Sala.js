55d8f43e1d3c9bcd376d3da1a0666fbb6f3bb84c
Laureano <laureanojuarez69@gmail.com>
2025-10-19 02:24:47 -0300

    feat(models): update Reserva, Sala, and Seat models to include movieId and salaId fields

--- cine-API/src/models/Sala.js
+++ cine-API/src/models/Sala.js
@@ -3,6 +3,7 @@ import sequelize from "../db.js";
 
 const Sala = sequelize.define("Sala", {
   salaNumber: { type: DataTypes.INTEGER, allowNull: false },
+  movieId: { type: DataTypes.INTEGER, allowNull: false },
 });
 
 export default Sala;
--- cine-API/src/models/Seat.js
+++ cine-API/src/models/Seat.js
@@ -5,6 +5,7 @@ const Seat = sequelize.define("Seat", {
   fila: { type: DataTypes.STRING, allowNull: false },
   numero: { type: DataTypes.INTEGER, allowNull: false },
   disponible: { type: DataTypes.BOOLEAN, defaultValue: true },
+  salaId: { type: DataTypes.INTEGER, allowNull: false },
 });
 
 export default Seat;
